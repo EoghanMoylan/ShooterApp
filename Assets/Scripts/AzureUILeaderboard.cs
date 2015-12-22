63c91c1cf1f0ef50d515acf682b5a891bd01a558
EoghanMoylan <eoghanmoylan@hotmail.com>
2015-12-22 12:17:27 +0000

    Added windows phone build. Currently developer version, so debug console can pop up from time to time

--- Assets/Scripts/AzureUILeaderboard.cs
+++ Assets/Scripts/AzureUILeaderboard.cs
@@ -97,10 +97,10 @@ public class AzureUILeaderboard : MonoBehaviour
     private string faction;
 
 	[SerializeField]
-    public string AzureEndPoint = ""; // Your Connection URL
+    public string AzureEndPoint = "https://federationvsalliance.azure-mobile.net/"; // Your Connection URL
 	
 	[SerializeField]
-    public string ApplicationKey = ""; // Your API Key
+    public string ApplicationKey = "LHNqFFmdDGXluZIbBGLBkCQJSDfQYk12"; // Your API Key
 
 	// Table items
 	public List<Leaderboard> _leaderboardItems = new List<Leaderboard>();
---
+++ Assets/Scripts/CapitalHealth.cs
@@ -0,0 +1,63 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CapitalHealth : MonoBehaviour {
+
+    //determines the capital ship script is attached to is alliance
+    public bool weAreAlliance;
+    //determines if player is alliance
+    private bool isAlliance;
+    public GameObject healthBar;
+    public int health = 100;
+    public GameObject deathExplosion;
+	
+	// Update is called once per frame
+	void Update () 
+    {
+        if (PlayerPrefs.GetInt("isAlliance") == 1)
+        {
+            isAlliance = true;
+        }
+        else
+        {
+            isAlliance = false;
+        }
+	  if(health == 0)
+        {
+            StartCoroutine(Die());
+        }
+	
+	}
+    private void OnCollisionEnter2D(Collision2D coll)
+    {
+        if(weAreAlliance != isAlliance)
+        {
+            if (coll.gameObject.tag == "Ammo")
+            {
+                health--;
+                UpdateHealthBar(0.01f);
+            }
+        }
+        
+    }
+    private void UpdateHealthBar(float myHealth)
+    {
+        healthBar.transform.localScale = new Vector3(healthBar.transform.localScale.x - myHealth, healthBar.transform.localScale.y, healthBar.transform.localScale.z);
+    }
+    IEnumerator Die()
+    {
+        //Saves the current score and level to be used in the "death scene"
+        int score = PlayerPrefs.GetInt("Score");
+        score += 500;
+        PlayerPrefs.SetInt("Score", score);
+        GameObject explosionClone;
+        explosionClone = (Instantiate(deathExplosion, gameObject.transform.position, Quaternion.identity)) as GameObject;
+        Destroy(explosionClone, 0.3f);
+        yield return new WaitForSeconds(0.2f);
+        Destroy(gameObject);
+        int waveNumber = PlayerPrefs.GetInt("WaveNumber");
+        waveNumber++;
+        PlayerPrefs.SetInt("WaveNumber", waveNumber);
+        Application.LoadLevel("Main");
+    }
+}
